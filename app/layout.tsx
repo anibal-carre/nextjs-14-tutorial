a660d5dc0672f8801bff4cfaf989d4d59c1560e7
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-05-14 13:43:21 -0300

    principal page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }
