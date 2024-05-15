6a8c3be9ba6fb710935ce749d08775ee603f0316
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-05-15 13:23:40 -0300

    layout and pages

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,8 @@
-import { Montserrat } from 'next/font/google';
+import { Montserrat, Lusitana } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  style: 'normal',
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
