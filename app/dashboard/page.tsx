07811472870a8d64dbbc0ebd92fe513297fb422e
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-05-17 13:29:05 -0300

    card data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 
 export default async function DashboardPage() {
   return (
@@ -14,14 +15,13 @@ export default async function DashboardPage() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* Se puede mostrar los datos separadamente en los cards si se hace el fetch en la pagina del dashboard o usar el cardwrapper y hacer el fecth dentro, igual las cards se van a ver}
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
         {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
         {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
+        {/* <Card title="Total Customers" value={1} type="customers" /> */}
+
+        <CardWrapper />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
