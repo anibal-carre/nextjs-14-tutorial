a681b74303e9daf4dac86423d45cdf90db05ab56
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-05-20 13:42:38 -0300

    delete invoice action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,14 +70,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
-  // Unreachable code block
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice' };
-  } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice' };
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
