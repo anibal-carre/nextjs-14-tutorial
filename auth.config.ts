35b1279f1e765a600223f90672300aadfbfcd48f
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-05-22 13:29:46 -0300

    authentication added

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import type { NextAuthConfig } from 'next-auth';
+import { signIn } from 'next-auth/react';
+import { pages } from 'next/dist/build/templates/app-page';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
